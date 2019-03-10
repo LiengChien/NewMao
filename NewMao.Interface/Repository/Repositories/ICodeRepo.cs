6cbce61a9b20a06aca86591e6c6b1dd49010d384
link87563 <b87563@gmail.com>
2019-03-11 00:03:05 +0800

    20190310-2

---
+++ NewMao.Interface/Repository/Repositories/ICodeRepo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewMao.Interface.Repository.Repositories
+{
+    interface ICodeRepo
+    {
+    }
+}
---
+++ NewMao.Repository/Repositories/CodeRepo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewMao.Repository.Repositories
+{
+    class CodeRepo
+    {
+    }
+}
