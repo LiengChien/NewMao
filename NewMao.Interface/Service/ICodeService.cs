65db5619a070fb9560e390c0183937116c58a1c2
link87563 <b87563@gmail.com>
2019-03-10 23:59:55 +0800

    20190310

---
+++ NewMao.Interface/Service/ICodeService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewMao.Interface.Service
+{
+    interface ICodeService
+    {
+    }
+}
---
+++ NewMao.Service/Services/CodeService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewMao.Service.Services
+{
+    class CodeService
+    {
+    }
+}
